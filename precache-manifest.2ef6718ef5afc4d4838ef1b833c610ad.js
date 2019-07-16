87ecfe592d78ebb43a4a8fcb8b1d00139be8dfbe
Kerem Sevencan <kerem@ritmix.org>
2019-07-16 17:54:46 +0200

    Updates

---
+++ precache-manifest.2ef6718ef5afc4d4838ef1b833c610ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad3916412b3a84dc73c43c042eb4ba4f",
+    "url": "/mini-kanban/index.html"
+  },
+  {
+    "revision": "ec2370a3f11bc6d03b79",
+    "url": "/mini-kanban/static/css/main.c796aa96.chunk.css"
+  },
+  {
+    "revision": "c51afece829b27182476",
+    "url": "/mini-kanban/static/js/2.3402338b.chunk.js"
+  },
+  {
+    "revision": "ec2370a3f11bc6d03b79",
+    "url": "/mini-kanban/static/js/main.e65c1226.chunk.js"
+  },
+  {
+    "revision": "b7dc0c7cfe3f2c475c10",
+    "url": "/mini-kanban/static/js/runtime~main.b28e786d.js"
+  }
+]);
